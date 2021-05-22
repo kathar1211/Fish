11de10e8a36dc8e35734d49fa466aa903ad0da3e
sarah <52550982+raaamen@users.noreply.github.com>
2021-05-21 22:47:45 -0400

    fish catalog almost done

--- Fish Game/Assets/Scripts/Catalog.cs
+++ Fish Game/Assets/Scripts/Catalog.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class Catalog : MonoBehaviour
 {
 
-    public List<Fish> fishList;
+    public List<string> fishCaughtList;
     public List<GameObject> panelList;
     public GameObject fishPanel;
     int index;
@@ -18,7 +18,6 @@ public class Catalog : MonoBehaviour
     {
        
         InitializePanel();
-        InitializeList();
     }
 
     // Update is called once per frame
@@ -30,7 +29,7 @@ public class Catalog : MonoBehaviour
     public void InitializePanel()
     {
         index = 0;
-        for (int i = 0; i < fishList.Count+1; i++)
+        for (int i = 0; i < 23; i++)
         {
             Debug.Log(index);
             panelList[i] = GameObject.Find("FishPos1 ("+index+")");
@@ -41,11 +40,81 @@ public class Catalog : MonoBehaviour
         }
     }
 
-    public void InitializeList()
+    public void fishCaught(Fish fishCaught)
     {
-        foreach (var item in fishList)
+        fishCaughtList.Add(fishCaught._name);
+        if (fishCaughtList.Contains(fishCaught._name))
         {
-
+            switch (fishCaught.name)
+            {
+                case "Salmon":
+                    panelList[11].GetComponent<Image>().sprite = fishCaught._fishSprite;
+                    break;
+                case "Tuna":
+                    panelList[20].GetComponent<Image>().sprite = fishCaught._fishSprite;
+                    break;
+                case "Pufferfish":
+                    panelList[10].GetComponent<Image>().sprite = fishCaught._fishSprite;
+                    break;
+                case "Eel":
+                    panelList[5].GetComponent<Image>().sprite = fishCaught._fishSprite;
+                    break;
+                case "Catfish":
+                    panelList[1].GetComponent<Image>().sprite = fishCaught._fishSprite;
+                    break;
+                case "Squid":
+                    panelList[16].GetComponent<Image>().sprite = fishCaught._fishSprite;
+                    break;
+                case "Octopus":
+                    panelList[8].GetComponent<Image>().sprite = fishCaught._fishSprite;
+                    break;
+                case "Baby Shark":
+                    panelList[0].GetComponent<Image>().sprite = fishCaught._fishSprite;
+                    break;
+                case "Vegan 'Sushi'":
+                    panelList[9].GetComponent<Image>().sprite = fishCaught._fishSprite;
+                    break;
+                case "Crab":
+                    panelList[3].GetComponent<Image>().sprite = fishCaught._fishSprite;
+                    break;
+                case "Flounder":
+                    panelList[6].GetComponent<Image>().sprite = fishCaught._fishSprite;
+                    break;
+                case "Sardine":
+                    panelList[12].GetComponent<Image>().sprite = fishCaught._fishSprite;
+                    break;
+                case "Starfish":
+                    panelList[17].GetComponent<Image>().sprite = fishCaught._fishSprite;
+                    break;
+                case "Stingray":
+                    panelList[18].GetComponent<Image>().sprite = fishCaught._fishSprite;
+                    break;
+                case "Sea Cucumber":
+                    panelList[13].GetComponent<Image>().sprite = fishCaught._fishSprite;
+                    break;
+                case "Clownfish":
+                    panelList[2].GetComponent<Image>().sprite = fishCaught._fishSprite;
+                    break;
+                case "Yellow Tang":
+                    panelList[21].GetComponent<Image>().sprite = fishCaught._fishSprite;
+                    break;
+                case "Dory?":
+                    panelList[4].GetComponent<Image>().sprite = fishCaught._fishSprite;
+                    break;
+                case "Trumpeter":
+                    panelList[19].GetComponent<Image>().sprite = fishCaught._fishSprite;
+                    break;
+                case "Sea Urchin":
+                    panelList[15].GetComponent<Image>().sprite = fishCaught._fishSprite;
+                    break;
+                case "Jellyfish":
+                    panelList[7].GetComponent<Image>().sprite = fishCaught._fishSprite;
+                    break;
+                case "Sea Turtle":
+                    panelList[14].GetComponent<Image>().sprite = fishCaught._fishSprite;
+                    break;
+            }
         }
     }
+
 }
--- Fish Game/Assets/Scripts/Fish.cs
+++ Fish Game/Assets/Scripts/Fish.cs
@@ -14,7 +14,6 @@ public class Fish : ScriptableObject
     //rarity will go from 1-10. 1 is least rare, 10 is most rare. figured this is easiest since we have 22 fish
     public int _rarity;
     public string _desc;
-
 }
 
 
